a34fbbb00576e9ab99770c195a9ec6f066cd6c3c
Arch me <kishudwivedi30@gmail.com>
2025-06-19 15:06:16 +0530

    admin created

---
+++ backend/app/models.py
@@ -0,0 +1,40 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from datetime import datetime
+from app import db
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.String(10), nullable=False)
+
+    reservations = db.relationship("Reservation", backref="user", lazy=True)
+
+
+class ParkingLot(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    prime_location_name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    address = db.Column(db.String(255), nullable=False)
+    pincode = db.Column(db.String(6), nullable=False)
+    number_of_spots = db.Column(db.Integer, nullable=False)
+
+    spots = db.relationship("ParkingSpot", backref="lot", cascade="all, delete-orphan", lazy=True)
+
+
+class ParkingSpot(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    lot_id = db.Column(db.Integer, db.ForeignKey('parking_lot.id'), nullable=False)
+    status = db.Column(db.String(1), default='A')  # A = Available, O = Occupied
+
+    reservation = db.relationship("Reservation", backref="spot", lazy=True, uselist=False)
+
+
+class Reservation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    spot_id = db.Column(db.Integer, db.ForeignKey('parking_spot.id'), nullable=False)
+    parking_timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    leaving_timestamp = db.Column(db.DateTime)
+    parking_cost = db.Column(db.Float)
--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,24 @@
-from app import create_app
+from app import create_app, db
+from app.models import User
+from werkzeug.security import generate_password_hash
 
 app = create_app()
+
+with app.app_context():
+    db.create_all()
+
+    admin = User.query.filter_by(username="admin").first()
+    if not admin:
+        admin = User(
+                username="admin",
+                password=generate_password_hash("admin123"),
+                role="admin"
+                )
+        db.session.add(admin)
+        db.session.commit()
+        print("Admin user created username='admin', password='admin123'")
+    else:
+        print("Admin user already exists.")
 
 if __name__ == "__main__":
     app.run(debug=True)
